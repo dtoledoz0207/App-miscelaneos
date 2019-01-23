252eb4c73b49a87f6fd7a121ccf6ab0d9ade4c85
David Toledo <dtoledoz0207@gmail.com>
2019-01-23 12:17:18 -0600

    routes-childrenRoutes-example-Added

---
+++ src/app/app.routes.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import {HomeComponent} from './components/home/home.component';
+import {UsuarioComponent} from './components/usuario/usuario.component';
+
+import {USUARIO_ROUTES} from './components/usuario/usuario.routes';
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { 
+        path: 'usuario/:id', 
+        component: UsuarioComponent,
+        children: USUARIO_ROUTES
+    },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(APP_ROUTES)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
