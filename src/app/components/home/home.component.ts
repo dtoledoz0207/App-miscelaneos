4828175b76307a9a45863ae137c91ef17a534cfb
David Toledo <dtoledoz0207@gmail.com>
2019-01-23 12:58:40 -0600

    lifeCycle-Component-Added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,24 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   template: `
     <app-ng-style></app-ng-style>
+    <br>
+
     <app-css></app-css>
+    <br>
 
     <app-clases></app-clases>
+    <br>
 
     <p [appResaltado]="'purple'">Hola mundo</p>
+    <br>
 
     <app-ng-switch></app-ng-switch>
   `,
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() { 
+    console.log("CONSTRUCTOR");
+  }
 
   ngOnInit() {
+    console.log("ngOnInit");
+  }
+
+
+  ngOnChanges(){
+    console.log("ngOnChanges");
+  }
+  ngDoCheck(){
+    console.log("ngDoCheck");
+  }
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit");
+  }
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked");
+  }
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit");
+  }
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked");
+  }
+  ngOnDestroy(){
+    console.log("ngOnDestroy");
   }
 
 }
--- src/app/directives/resaltado.directive.ts
+++ src/app/directives/resaltado.directive.ts
@@ -7,7 +7,7 @@ export class ResaltadoDirective {
 
   constructor(private el:ElementRef) {
 
-    console.log("Directiva llamada");
+    //console.log("Directiva llamada");
     // el.nativeElement.style.backgroundColor = "yellow";
 
    }
